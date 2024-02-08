a77eb2f34b15fe54f8083eea4a99de082eff2fe6
David Smiles <david.smiles@outlook.com>
2024-02-08 19:25:07 +0000

    first commit

---
+++ src/test/java/com/github/dsmiles/springboottestingproject/SpringBootTestingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dsmiles.springboottestingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
